497f8ef30b7879a158e623987bebb914159c6af3
Jocelyn Tan <jocelyntan89@hotmail.com>
2017-10-28 21:12:47 +0800

    fixed API CORS issue

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-document.addEventListener('DOMContentLoaded', init)
+$(document).ready(function () {
 
-function init () {
+  // randomise the class called by its index from 1 - 12
+  var index = Math.floor((Math.random() * 10) + 3).toString()
 
-}
+  // get data from API
+  $.get('http://www.dnd5eapi.co/api/classes/' + index).done(function (data) {
+    console.log(data)
+  })
+})
